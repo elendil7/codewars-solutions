c407abbdfef27717333a15c151614359f151ac04
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2022-02-21 16:21:59 +0000

    Add several 8kyu katas

---
+++ JavaScript/8kyu/Fake Binary.js
@@ -0,0 +1,6 @@
+function fakeBin(x) {
+	return x
+		.split('')
+		.map((v) => (v < 5 ? '0' : '1'))
+		.join('');
+}
---
+++ JavaScript/8kyu/Hello, Name or World!.js
@@ -0,0 +1,7 @@
+function hello(name) {
+	return !name || name == ''
+		? 'Hello, World!'
+		: `Hello, ${
+				name.toUpperCase().slice(0, 1) + name.toLowerCase().slice(1)
+		  }!`;
+}
