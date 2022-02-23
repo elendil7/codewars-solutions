c9e58a8c082d6f4ce44ddcbda56bdd926215f3f3
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2022-02-23 19:04:06 +0000

    Add 8kyu katas (javascript)

---
+++ JavaScript/8kyu/Drink about.js
@@ -0,0 +1,8 @@
+peopleWithAgeDrink = (a) =>
+	a > 20
+		? 'drink whisky'
+		: a > 17
+		? 'drink beer'
+		: a > 13
+		? 'drink coke'
+		: 'drink toddy';
---
+++ JavaScript/8kyu/Grasshopper - Messi Goals.js
@@ -0,0 +1,5 @@
+var laLigaGoals = 43;
+var championsLeagueGoals = 10;
+var copaDelReyGoals = 5;
+
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
