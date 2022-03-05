9f538a5dd59d63d208d22a2cc50e11ac3b8683d8
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2022-03-05 00:41:52 +0000

    Add variations to previously solved katas

--- JavaScript/8kyu/Reversed sequence.js
+++ JavaScript/8kyu/Reversed sequence.js
@@ -3,3 +3,12 @@ const reverseSeq = (n) => {
 		.map((v) => v + 1)
 		.reverse();
 };
+
+reverseSeq = (n) =>
+	Array.from(Array(n).keys())
+		.map((v) => v + 1)
+		.reverse();
+
+reverseSeq = (n) => [...Array(n).keys()].map((v) => v + 1).reverse();
+
+reverseSeq = (n) => new Array(n).fill(n).map((c, i) => c - i);
