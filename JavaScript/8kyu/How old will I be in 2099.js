b3942761d3f270278e989e5209879d0d92789234
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2022-03-11 20:06:24 +0000

    Add daily katas

---
+++ JavaScript/8kyu/How old will I be in 2099.js
@@ -0,0 +1,13 @@
+calculateAge = (y, n) =>
+	n - y < 0
+		? `You will be born in ${y - n} ${y - n == 1 ? `year` : `years`}.`
+		: n - y == 0
+		? `You were born this very year!`
+		: `You are ${n - y} ${n - y == 1 ? `year` : `years`} old.`;
+
+calculateAge = (y, n) =>
+	n - y < 0
+		? `You will be born in ${y - n} year${y - n == 1 ? `` : `s`}.`
+		: n - y == 0
+		? `You were born this very year!`
+		: `You are ${n - y} year${n - y == 1 ? `` : `s`} old.`;
