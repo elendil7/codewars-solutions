c723d7826cb6aa13af4e26ee7f44910d554ddde1
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2022-03-14 18:15:33 +0000

    Added daily kata challenges

---
+++ JavaScript/7kyu/Nth power rules them all!.js
@@ -0,0 +1,5 @@
+modifiedSum = (a, n) =>
+	a.map((v) => v ** n).reduce((a, b) => a + b) - a.reduce((a, b) => a + b);
+
+modifiedSum = (a, n) =>
+	a.reduce((a, b) => a + b ** n, 0) - a.reduce((a, b) => a + b);
