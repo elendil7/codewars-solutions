0fcce2ebdb46db118f09eaf04315b8b20d3e935a
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2022-03-19 20:12:49 +0000

    Added daily katas

---
+++ JavaScript/8kyu/For UFC Fans (Total Beginners) - Conor McGregor vs George Saint Pierre.js
@@ -0,0 +1,4 @@
+quote = (f, x = f.toLowerCase()) =>
+	x == `george saint pierre`
+		? `I am not impressed by your performance.`
+		: `I'd like to take this chance to apologize.. To absolutely NOBODY!`;
