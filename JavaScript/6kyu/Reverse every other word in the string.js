1b0987163064714195a0d91b987a605e836618d3
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2022-03-23 19:52:44 +0000

    Add even more daily katas

---
+++ JavaScript/6kyu/Reverse every other word in the string.js
@@ -0,0 +1,3 @@
+reverse = (s) =>
+	s.split` `.map((v, i) => (i % 2 ? [...v].reverse().join`` : v))
+		.join` `.trim();
---
+++ JavaScript/7kyu/Indexed capitalization.js
@@ -0,0 +1,2 @@
+capitalize = (s, a) =>
+	[...s].map((v, i) => (a.includes(i) ? v.toUpperCase() : v)).join``;
