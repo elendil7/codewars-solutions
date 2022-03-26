79894baedb3645d74b2a196fd6a028a802dc0fdc
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2022-03-26 14:37:30 +0000

    Add daily katas

---
+++ JavaScript/6kyu/If you can read this....js
@@ -0,0 +1,3 @@
+to_nato = (w) =>
+	w.split` `.map((v) => [...v.toLowerCase()].map((x) => NATO[x] || x).join` `)
+		.join` `;
---
+++ JavaScript/7kyu/Simple Fun #190 - Folding Paper.js
@@ -0,0 +1,6 @@
+folding = (a, b, n) =>
+	a == b
+		? n || 1
+		: a > b
+		? folding(a - b, b, !n ? 2 : ++n)
+		: folding(a, b - a, !n ? 2 : ++n);
