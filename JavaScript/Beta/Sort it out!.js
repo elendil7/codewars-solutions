6786d741d77241925be3739545c86e3df800d224
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2022-03-31 17:57:52 +0100

    Add daily beta katas

---
+++ JavaScript/Beta/Sort it out!.js
@@ -0,0 +1,11 @@
+function sortCollection(arr) {
+	if (arr === undefined) return undefined;
+	if (arr === null) return null;
+	if (!arr[0]) return [];
+
+	return arr.sort((a, b) =>
+		a.rating == b.rating
+			? a.film.localeCompare(b.film)
+			: a.rating < b.rating
+	);
+}
