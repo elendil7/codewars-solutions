3dec02e848ba95f731027e953b4bdb1f534bd42d
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2022-04-01 15:20:19 +0100

    Add daily katas

---
+++ JavaScript/7kyu/Incrementer.js
@@ -0,0 +1,5 @@
+function incrementer(nums) {
+	return nums.map((v, i) =>
+		((x) => ((`` + x).length > 1 ? +(`` + x)[1] : x))(v + i + 1)
+	);
+}
---
+++ JavaScript/7kyu/Odd-Even String Sort.js
@@ -0,0 +1,6 @@
+function sortMyString(S) {
+	return [
+		S.split``.filter((v, i) => !(i % 2)).join``,
+		S.split``.filter((v, i) => i % 2).join``,
+	].join` `;
+}
