0580f4165e37267c1332c8daa1f88371147399fe
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2022-04-13 21:24:42 +0100

    Added 22 daily katas

---
+++ JavaScript/7kyu/How many times should I go.js
@@ -0,0 +1,17 @@
+function howManyTimes(annualPrice, individualPrice, currentPrice, count) {
+	if (individualPrice === 0) return Infinity;
+
+	if (currentPrice == undefined) currentPrice = 0;
+	if (count == undefined) count = 0;
+
+	if (annualPrice <= currentPrice) {
+		return count;
+	} else {
+		return howManyTimes(
+			annualPrice,
+			individualPrice,
+			currentPrice + individualPrice,
+			count + 1
+		);
+	}
+}
