f21a93147e6b4fb883a09eee790ae49ce2f1a9ee
Aragorn <80429382+elendil7@users.noreply.github.com>
2022-04-18 21:47:35 +0100

    Create Kadafi vs. Armani.js

---
+++ JavaScript/Kumite/Kadafi vs. Armani.js
@@ -0,0 +1,1 @@
+find_unique_items=(a,b)=>b.includes((1000/200-1)**2/2)?(x=>[...`${x.slice(-1)+x.slice(0,-1)}`].map(v=>~~v).filter(v=>v))(Array.from(Array(8).keys()).map(v=>++v).filter(v=>++v%2).concat(1)):[...Array(8)].fill(8).map((v,i)=>v-i).reverse()
