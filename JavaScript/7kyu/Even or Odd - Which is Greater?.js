9e21d840784ef40cc6cfea29a50197be04d84de0
Aragorn <80429382+elendil7@users.noreply.github.com>
2022-04-19 00:55:59 +0100

    Create Even or Odd - Which is Greater?.js

---
+++ JavaScript/7kyu/Even or Odd - Which is Greater?.js
@@ -0,0 +1,12 @@
+function evenOrOdd(str) {
+  let arr = str.split``
+  
+  let even = arr.filter(v=>+v%2==0)
+  
+  let odd = arr.filter(v=>+v%2)
+  
+  let addedEvens = even.reduce((a,b)=>+a + +b,0)
+  let addedOdds = odd.reduce((a,b)=>+a + +b,0)
+  
+  return addedEvens > addedOdds ? `Even is greater than Odd` : addedOdds > addedEvens ? `Odd is greater than Even` : `Even and Odd are the same`
+}
