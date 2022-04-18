2b3da3bfea17f53201cdd7d2002b46d5e6f5829f
Aragorn <80429382+elendil7@users.noreply.github.com>
2022-04-18 23:07:57 +0100

    Create Sleigh Authentication.js

---
+++ JavaScript/8kyu/Sleigh Authentication.js
@@ -0,0 +1,7 @@
+function Sleigh() {}
+Sleigh.prototype.authenticate = function(name, password) {
+   return name==`Santa Claus`&password==`Ho Ho Ho!`?!0:!1
+}
+
+function Sleigh() {}
+Sleigh.prototype.authenticate=(n,p)=>n==`Santa Claus`&&p==`Ho Ho Ho!`
