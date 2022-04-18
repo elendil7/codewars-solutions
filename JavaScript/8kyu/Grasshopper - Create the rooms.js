db98b506453c4977e3503c711faccdb5761b312a
Aragorn <80429382+elendil7@users.noreply.github.com>
2022-04-19 00:06:25 +0100

    Create Grasshopper - Create the rooms.js

---
+++ JavaScript/8kyu/Grasshopper - Create the rooms.js
@@ -0,0 +1,18 @@
+// Add rooms here
+var rooms = {
+  livingRoom: {
+    name: ``,
+    description: ``,
+    completed: ``
+  },
+  bathRoom: {
+    name: ``,
+    description: ``,
+    completed: ``
+  },
+  bedRoom: {
+    name: ``,
+    description: ``,
+    completed: ``
+  }
+}
