15c972401b8a07aa42f7a393d243d89fbc04a1c3
Aragorn <80429382+elendil7@users.noreply.github.com>
2022-04-18 22:24:55 +0100

    Create Categorize New Member.js

---
+++ JavaScript/7kyu/Categorize New Member.js
@@ -0,0 +1,5 @@
+const openOrSenior=(d)=>d.map((v,i)=>v[0]>54&&v[1]>7?"Senior":"Open")
+
+const openOrSenior=(d)=>d.map((v)=>v[0]>54&&v[1]>7?"Senior":"Open")
+
+const openOrSenior=(d)=>d.map(v=>v[0]>54&&v[1]>7?"Senior":"Open")
