e5a99726000d51edd4d74adef70232db92beb59b
Aragorn <80429382+elendil7@users.noreply.github.com>
2022-04-19 00:45:41 +0100

    Create Freudian translator .js

---
+++ JavaScript/8kyu/Freudian translator .js
@@ -0,0 +1,6 @@
+function toFreud(str) {
+  let alphabet = `abcdefghijklmnopqrstuvwxyz`.split``
+  let thing = `${alphabet[18]}${alphabet[4]}${alphabet[23]}`
+  
+  return str[0] ? str.split` `.map(v=>thing).join` ` : ``
+}
