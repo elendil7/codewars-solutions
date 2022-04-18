7da868d52dfdeec1d35cb0cbba4668fc2f4d5797
Aragorn <80429382+elendil7@users.noreply.github.com>
2022-04-18 22:41:26 +0100

    Create Multiplication table for number.js

---
+++ JavaScript/8kyu/Multiplication table for number.js
@@ -0,0 +1,3 @@
+multiTable=n=>Array.from(Array(10).keys()).map((v,i)=>`${i+1} * ${n} = ${(i+1)*n}`).join("\n")
+
+multiTable=n=>Array.from(Array(10).keys()).map((v,i)=>`${i+1} * ${n} = ${(i+1)*n}`).join("\n")
