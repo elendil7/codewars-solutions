3317e96d9f44626fba2859ad7ad9d1aab384fa55
Aragorn <80429382+elendil7@users.noreply.github.com>
2022-04-18 22:39:00 +0100

    Create Twice as old.js

---
+++ JavaScript/8kyu/Twice as old.js
@@ -0,0 +1,8 @@
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  console.log(dadYearsOld, sonYearsOld)
+  return Math.abs(dadYearsOld - sonYearsOld*2)
+}
+
+twiceAsOld=(d,s)=>Math.abs(d-s*2)
+
+twiceAsOld=(d,s)=>Math.abs(d-s*2)
