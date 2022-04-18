a382a6dc5943bf75e3ab0a957c4461abc8b107ab
Aragorn <80429382+elendil7@users.noreply.github.com>
2022-04-19 00:09:22 +0100

    Create Stop worrying!.js

---
+++ JavaScript/Beta/Stop worrying!.js
@@ -0,0 +1,4 @@
+function worries(str){
+  let arr = ["Making last-minute changes","Sitting in a dark room and shaking","Crying","Laughing hysterically","Not eating"]
+  return arr.includes(str) ? "Stop worrying, everything will be all right!" : "Do you really care?"
+}
