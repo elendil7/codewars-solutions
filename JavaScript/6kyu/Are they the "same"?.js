809013eb3846c11b3c0628998d3ed95a8e3d9697
Aragorn <80429382+elendil7@users.noreply.github.com>
2022-04-18 22:16:00 +0100

    Create Are they the "same"?.js

---
+++ JavaScript/6kyu/Are they the "same"?.js
@@ -0,0 +1,6 @@
+function comp(a1, a2){  
+  if(!a1 || !a2 || a1.length !== a2.length) return false;    
+  return a1.map(v => v * v).sort((a,b)=>a-b).toString() === a2.sort((a,b)=>a-b).toString();
+}
+
+// return a1.map(v => v === Math.sqrt(a2[a1.indexOf(v)]))
