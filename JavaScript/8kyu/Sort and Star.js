7ce99e6bcbdcd61831b09c1b8c6efb44026bb8ee
Aragorn <80429382+elendil7@users.noreply.github.com>
2022-04-18 22:56:34 +0100

    Create Sort and Star.js

---
+++ JavaScript/8kyu/Sort and Star.js
@@ -0,0 +1,9 @@
+twoSort=(s)=>{
+  return s.sort()[0].split(``).join(`***`)
+}
+
+/*
+
+return s.sort((a,b)=>a.toLowerCase().localeCompare(b.toLowerCase())).slice(0,1).join("").split("").join("***")
+
+*/
