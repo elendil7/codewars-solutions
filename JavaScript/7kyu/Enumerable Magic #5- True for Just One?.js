758afca954b9eaa980c33aeec2e67161e45b1f23
Aragorn <80429382+elendil7@users.noreply.github.com>
2022-04-19 00:46:43 +0100

    Create Enumerable Magic #5- True for Just One?.js

---
+++ JavaScript/7kyu/Enumerable Magic #5- True for Just One?.js
@@ -0,0 +1,15 @@
+function one(arr, fun){
+  let count = 0
+  
+  for(let i = 0; i < arr.length; ++i){
+    if(fun(arr[i])){
+      count++
+    }
+    if(count > 1){
+      return false
+    }
+  }
+  
+  if(count < 1) return false
+  return true
+}
