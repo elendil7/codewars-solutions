d34d33ff2602b4092e08af3914c6da0904679fc6
Aragorn <80429382+elendil7@users.noreply.github.com>
2022-04-18 23:18:02 +0100

    Create The Wide-Mouthed frog! .js

---
+++ JavaScript/8kyu/The Wide-Mouthed frog! .js
@@ -0,0 +1,9 @@
+function mouthSize(a) {
+  console.log(a)
+  
+  return a.toLowerCase()==`alligator`?`small`:`wide`
+}
+
+function mouthSize(a) {
+  return a.toLowerCase() == `alligator` ? `small` : `wide`;
+}
