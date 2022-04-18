3f5c4f491b85cf73cb5c5f60e25675c4fd886032
Aragorn <80429382+elendil7@users.noreply.github.com>
2022-04-18 22:03:58 +0100

    Create A Needle in the Haystack.js

---
+++ JavaScript/8kyu/A Needle in the Haystack.js
@@ -0,0 +1,7 @@
+function findNeedle(h) {
+  return `found the needle at position ${h.findIndex(e => e === `needle`)}`
+}
+
+function findNeedle(h) {
+  return `found the needle at position ${h.indexOf(`needle`)}`
+}
