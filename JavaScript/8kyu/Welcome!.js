0387b4fc95d17da479ba20d4540204f092512d3f
Aragorn <80429382+elendil7@users.noreply.github.com>
2022-04-18 22:27:46 +0100

    Create Welcome!.js

---
+++ JavaScript/8kyu/Welcome!.js
@@ -0,0 +1,25 @@
+greet=(l)=>{
+
+  let o={
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso'
+  }
+  
+  return !o[l]?"Welcome":o[l]
+
+}
