7a253fbfa7316a7615055a8acd73185ffa6d179c
Aragorn <80429382+elendil7@users.noreply.github.com>
2022-12-11 04:14:02 +0000

    Update Count IP Addresses.js

--- JavaScript/5kyu/Count IP Addresses.js
+++ JavaScript/5kyu/Count IP Addresses.js
@@ -3,3 +3,5 @@ function ipsBetween(start, end){
   const [e1, e2, e3, e4] = end.split(".")
   return  16777216*(e1-s1) + 65536*(e2-s2) + 256*(e3-s3) + 1*(e4-s4)
 }
+
+ipsBetween=(x,y,[a,b,c,d,e,f,g,h]=x.concat(`.`+y).split`.`)=>16777216*(e-a)+65536*(f-b)+256*(g-c)+(h-d)
